784b0da74298a80d89b867cdd3937d4655c73660
Aklesh888 <aklesh.dahal@gmail.com>
2026-01-29 00:27:36 +0545

    started login page UI

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router";
+import { Login } from "./routes/login";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
-)
+);
---
+++ client/src/routes/login.jsx
@@ -0,0 +1,133 @@
+import styles from "../styles/Login.module.css";
+import swedenFlag from "../assets/login/SwedenFlag.png";
+import ukFlag from "../assets/login/UKFlag.png";
+import logo from "../assets/login/Diamond.png";
+import { useState } from "react";
+
+export const Login = () => {
+  const [selectedLanguage, setSelectedLanguage] = useState("Language 2");
+  const [isLanguageSelectorOpen, setIsLanguageSelectorOpen] = useState(true);
+
+  const [showHamburgerMenu, setShowHamburgerMenu] = useState(false);
+
+  const handleLanguageButton = (language) => {
+    setSelectedLanguage(language);
+    setIsLanguageSelectorOpen(false);
+  };
+
+  return (
+    <div className={styles.background}>
+      <nav className={styles.navbarContainer}>
+        <div className={styles.logoContainer}>
+          <img src={logo} alt="" className={styles.logo} />
+        </div>
+        <div
+          className={styles.hamburger}
+          onClick={() => {
+            setShowHamburgerMenu(!showHamburgerMenu);
+          }}
+        >Open</div>
+
+        <div className={styles.navbarLinks}>
+          <div className={styles.navbarLink}>Home</div>
+          <div className={styles.navbarLink}>Home</div>
+          <div className={styles.navbarLink}>Home</div>
+          <div className={styles.navbarLink}>Home</div>
+          <div className={styles.navbarLink}>Home</div>
+
+          {isLanguageSelectorOpen && (
+            <div className={styles.selectLanguageContainer}>
+              <div
+                className={styles.languageSelect}
+                onClick={() => handleLanguageButton("Language 1")}
+              >
+                <div>Language 1</div>
+                <img src={swedenFlag} alt="" className={styles.flagImage} />
+              </div>
+              <div
+                className={styles.languageSelect}
+                onClick={() => handleLanguageButton("Language 2")}
+              >
+                <div>Language 2</div>
+                <img src={ukFlag} alt="" className={styles.flagImage} />
+              </div>
+            </div>
+          )}
+
+          {selectedLanguage === "Language 1" ? (
+            <div
+              className={styles.languageSelect}
+              onClick={() => {
+                setIsLanguageSelectorOpen(true);
+              }}
+            >
+              <div>Language 1</div>
+              <img src={swedenFlag} alt="" className={styles.flagImage} />
+            </div>
+          ) : (
+            <div
+              className={styles.languageSelect}
+              onClick={() => {
+                setIsLanguageSelectorOpen(true);
+              }}
+            >
+              <div>Language 2</div>
+              <img src={ukFlag} alt="" className={styles.flagImage} />
+            </div>
+          )}
+        </div>
+      </nav>
+      
+      {showHamburgerMenu && (
+        <div className={styles.hamburgerContainer}>
+          <div className={styles.hamburgerItem}>Home</div>
+          <div className={styles.hamburgerItem}>Home</div>
+          <div className={styles.hamburgerItem}>Home</div>
+          <div className={styles.hamburgerItem}>Home</div>
+          <div className={styles.hamburgerItem}>Home</div>
+        </div>
+      )}
+
+      <div className={styles.loginContainer}>
+        <div className={styles.login}>
+          <div className={styles.loginTitle}>Log in</div>
+          <div className={styles.inputContainer}>
+            <div className={styles.inputLabel}>Enter your email address</div>
+            <input
+              type="text"
+              className={styles.email}
+              placeholder="Email address"
+            />
+          </div>
+          <div className={styles.inputContainer}>
+            <div className={styles.inputLabel}>Enter your password address</div>
+            <input
+              type="text"
+              className={styles.password}
+              placeholder="Password"
+            />
+          </div>
+          <button className={styles.loginButton}>Login</button>
+
+          <div className={styles.registerForgetContainer}>
+            <button className={styles.registerButton}>Register</button>
+            <button className={styles.forgotButton}>Forgot Password</button>
+          </div>
+        </div>
+      </div>
+
+      <footer className={styles.footerContainer}>
+        <div className={styles.footerTop}>
+          <div className={styles.companyName}>123 Fakturera</div>
+          <div className={styles.footerLinks}>
+            <div>Home</div>
+            <div>Order</div>
+            <div>Contact Us</div>
+          </div>
+        </div>
+        <hr className={styles.line} />
+        <div>© Lättfaktura, CRO no. 638537, 2025. All rights reserved.</div>
+      </footer>
+    </div>
+  );
+};
